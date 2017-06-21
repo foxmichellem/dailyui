341ab77caef44487b7ccdbe3df546debf05c0abc
Michelle Fox <fox.michelle.m@gmail.com>
2017-06-20 22:38:50 -0700

    add styling for a sidebar

--- src/App.js
+++ src/App.js
@@ -4,31 +4,30 @@ import Signup from './Signup';
 import Home from './Home';
 import { Route, Switch, Link } from 'react-router-dom'
 
-const Header = () => (
-  <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/signup'>Signup</Link></li>
-      </ul>
-    </nav>
-  </header>
+const Navigation = () => (
+  <div className="sidebar">
+    <h2>UI Components</h2>
+    <ul>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/signup'>Signup</Link></li>
+    </ul>
+  </div>
 )
 
 const Main = () => (
-  <main>
+  <div className="content">
     <Switch>
       <Route exact path='/' component={Home}/>
       <Route path='/signup' component={Signup}/>
     </Switch>
-  </main>
+  </div>
 )
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+        <Navigation />
         <Main />
       </div>
     );
