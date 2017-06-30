728445619a8a3e173c1ffcc28914616f5ce5fba9
Michelle Fox <fox.michelle.m@gmail.com>
2017-06-29 18:02:42 -0700

    WIP

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -1,42 +1,58 @@
 import React from 'react';
-import './Signup.css'
+import './Signup.css';
+import './../Grid.css';
+var createReactClass = require('create-react-class');
 
 
-var Input = React.createClass({
+var Input = createReactClass({
   render: function(){
     return (
       <div className="input">
-        <input id={this.props.name} required type={this.props.type} placeholder={this.props.placeholder} />
+        <input className="input__box" id={this.props.name} type={this.props.type} placeholder={this.props.placeholder} />
       </div>
     );
   }
 });
 
-var Modal = React.createClass({
+var Modal = createReactClass({
+  handleSubmit: function(e){
+    console.log('handled');
+  },
+
+
   render: function(){
     return (
       <div className="modal">
-        <form onSubmit={this.props.onFormSubmit} className="modal">
-          <Input id="name" type="text" placeholder="Full name" />
-          <Input id="email" type="email" placeholder = "Email" />
-          <Input id="password" type="password" placeholder = "Password" />
-          <Input id="password-confirm" type="password" placeholder = "Password" />
-          <button>Sign up</button>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-xs-12">
+              <form onSubmit={this.props.onFormSubmit} className="modal__form">
+                <Input id="name" type="text" placeholder="Full name" onChange={this.handleChange} />
+                <Input id="email" type="email" placeholder = "Email" onChange={this.handleChange} />
+                <Input id="password" type="password" placeholder = "Password" onChange={this.handleChange} />
+                <Input id="password-confirm" type="password" placeholder = "Password" onChange={this.handleChange} />
+                <button onClick={this.handleSubmit} className="modal__button">Sign up</button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 })
 
-var Signup = React.createClass({
+var Signup = createReactClass({
   render: function(){
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>This is the signup page</h2>
+      <div className="Signup">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-xs-12 center-xs">
+              <h2 className="page-title">Challenge #001 Sign up</h2>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Modal />
       </div>
     );
   }
